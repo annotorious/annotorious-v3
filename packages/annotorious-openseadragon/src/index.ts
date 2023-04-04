d38ff821e5e55565791112b80b0b5dd5eee2db1e
Rainer Simon <rainer@rainersimon.io>
2023-04-04 19:19:43 +0200

    More build cleanup

--- packages/annotorious-openseadragon/src/index.ts
+++ packages/annotorious-openseadragon/src/index.ts
@@ -2,3 +2,4 @@ export * from './Annotorious';
 
 // Re-export essential types
 export * from '@annotorious/core/src/model';
+export type { AnnotoriousOptions, Style }  from '@annotorious/annotorious/src/AnnotoriousOptions';
