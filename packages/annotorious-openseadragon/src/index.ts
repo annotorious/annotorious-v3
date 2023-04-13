0c68da87017a98dd135aacbbc5fee3ba1de828ef
Rainer Simon <rainer@rainersimon.io>
2023-04-13 15:28:41 +0200

    Added presence types to those that are re-exported by annotorious/openseadragon

--- packages/annotorious-openseadragon/src/index.ts
+++ packages/annotorious-openseadragon/src/index.ts
@@ -2,4 +2,5 @@ export * from './Annotorious';
 
 // Re-export essential types
 export * from '@annotorious/core/src/model';
+export * from '@annotorious/core/src/presence';
 export type { AnnotoriousOptions, Style }  from '@annotorious/annotorious/src/AnnotoriousOptions';
