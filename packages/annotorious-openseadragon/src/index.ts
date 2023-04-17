7ef6b42a8a9e2eb22f2a59b39044d402515f839e
Rainer Simon <rainer@rainersimon.io>
2023-04-17 12:33:54 +0200

    Core library build config

--- packages/annotorious-openseadragon/src/index.ts
+++ packages/annotorious-openseadragon/src/index.ts
@@ -1,6 +1,1 @@
 export * from './Annotorious';
-
-// Re-export essential types
-export * from '@annotorious/core/src/model';
-export * from '@annotorious/core/src/presence';
-export type { AnnotoriousOptions, Style }  from '@annotorious/annotorious/src/AnnotoriousOptions';
