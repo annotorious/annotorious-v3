d30e857dd4b124144adf5fdf14392903376f35a2
Rainer Simon <rainer@rainersimon.io>
2023-05-02 15:06:04 +0200

    Better placement of presence label for polygons

--- packages/annotorious-openseadragon/src/svg/presence/SVGPresenceLabel.svelte
+++ packages/annotorious-openseadragon/src/svg/presence/SVGPresenceLabel.svelte
@@ -9,42 +9,47 @@
 
   export let scale: number;
 
+  export let hAlign: 'CENTER' | null = null;
+
   let g: SVGGElement;
 
   $: if (g) setWidth(scale);
 
   const setWidth = (scale: number) => {
     const textEl = g.querySelector('text');
-    const rectEl = Array.from(g.querySelectorAll('rect'));
+    const rectEl = g.querySelector('rect');
 
     const width = textEl.getBBox().width + 10 / scale;
-    rectEl.forEach(r => r.setAttribute('width', `${width}`));
+
+    if (hAlign === 'CENTER') {
+      g.setAttribute('style', `transform: translateX(-${width / 2}px)`);
+    }
+
+    rectEl.setAttribute('width', `${width}`);
   }
 </script>
 
-<g class="a9s-presence-label" bind:this={g}>
+<g bind:this={g} class="a9s-presence-label">
   <rect
-    class="a9s-presence-label-bg-bottom" 
-    x={x + Math.round(2 / scale)} 
-    y={y - 10 / scale} 
-    height={10 / scale} 
-    fill={user.color}/>
+    class="a9s-presence-label-bg" 
+    x={x} 
+    y={y - 18 / scale} 
+    height={18 / scale} 
+    fill={user.color}
+    stroke={user.color} />
 
-  <rect
-    class="a9s-presence-label-bg-top" 
-    x={x + Math.round(2 / scale)} 
-    rx={5 / scale}
-    y={y - 20 / scale} 
-    ry={5 / scale}
-    height={14 / scale} 
-    fill={user.color}/>
-
-  <text font-size={12 / scale} x={x + Math.round(6 / scale)} y={y - 7 / scale}>{user.user.email}</text>
+  <text font-size={12 / scale} x={x + Math.round(5 / scale)} y={y - 5 / scale}>{user.user.email}</text>
 </g>
 
 <style>
   text {
     fill: #fff;
     font-family: Arial, Helvetica, sans-serif;
+    font-weight: 600;
+  }
+
+  rect {
+    stroke-width: 1.2;
+    vector-effect: non-scaling-stroke;
   }
 </style>
--- packages/annotorious-openseadragon/src/svg/presence/shapes/PresencePolygon.svelte
+++ packages/annotorious-openseadragon/src/svg/presence/shapes/PresencePolygon.svelte
@@ -1,6 +1,7 @@
 <script type="ts">
   import type { PresentUser } from '@annotorious/core';
   import type { ImageAnnotation, PolygonGeometry } from '@annotorious/annotorious';
+  import SVGPresenceLabel from '../SVGPresenceLabel.svelte';
 
   export let annotation: ImageAnnotation;
 
@@ -9,9 +10,28 @@
   export let scale: number;
 
   $: geom = annotation.target.selector.geometry as PolygonGeometry;
+
+  $: origin = getHighestPoint(geom);
+
+  const getHighestPoint = (geom: PolygonGeometry) => {
+    let [highestPoint, ...rest] = geom.points;
+
+    rest.forEach(([x, y]) => {
+      if (y < highestPoint[1])
+        highestPoint = [x,y];
+    });
+
+    return highestPoint;
+  }
 </script>
 
 <g class="a9s-presence-overlay">
+  <SVGPresenceLabel 
+    scale={scale} 
+    user={user} 
+    x={origin[0]} y={origin[1]} 
+    hAlign="CENTER" />
+  
   <polygon
     class="a9s-presence-shape a9s-presence-polygon"
     stroke={user.color}
