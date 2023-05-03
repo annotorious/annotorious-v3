42ecb23f7fa4fd930b22cf59f076ce2eb6e986d9
Rainer Simon <rainer@rainersimon.io>
2023-05-03 11:08:47 +0200

    Popup now follows viewer animation + annotation shape change

--- packages/annotorious-openseadragon/src/popup/Popup.svelte
+++ packages/annotorious-openseadragon/src/popup/Popup.svelte
@@ -1,9 +1,9 @@
 <script type="ts">
   import { onMount } from 'svelte';
-  import type OpenSeadragon from 'openseadragon';
   import { draggable } from '@neodrag/svelte';
-  import type { ImageAnnotationStore } from '@annotorious/annotorious';
-  import { defaultStrategy } from './PlacementStrategy';
+  import OpenSeadragon from 'openseadragon';
+  import type { StoreChangeEvent } from '@annotorious/core';
+  import type { ImageAnnotation, ImageAnnotationStore } from '@annotorious/annotorious';
 
   export let store: ImageAnnotationStore;
 
@@ -11,52 +11,89 @@
 
   const { selection } = store; 
 
-  let lastPointerDown: PointerEvent;
-
   let left: number;
 
   let top: number;
 
-  const dragOpts = {
-    ignoreMultitouch: true,
+  let dragged = false;
 
-    onDragStart: () => {
-      viewer.setMouseNavEnabled(false);
-    },
+  let storeObserver: (event: StoreChangeEvent<ImageAnnotation>) => void;
 
-    onDragEnd: () => {
-      viewer.setMouseNavEnabled(true);
-    }
+  const onDragStart = () => {
+    dragged = true;
+    viewer.setMouseNavEnabled(false);
   }
 
-  $: if ($selection) setPosition($selection);
+  const onDragEnd = () => {
+    viewer.setMouseNavEnabled(true);
+  }
+
+  $: $selection, onSelect();
+
+  const onSelect = () => {
+    if (storeObserver)
+      store.unobserve(storeObserver);
+
+    if ($selection) {
+      console.log('resetting selection');
+      dragged = false;
+      setPosition($selection);
+
+      storeObserver = (event: StoreChangeEvent<ImageAnnotation>) => {
+        if (!dragged)
+          setPosition($selection);
+      }
+
+      store.observe(storeObserver, { annotations: $selection });
+    } else {
+      console.log('deselect');
+    }
+  }
 
   const setPosition = (selection: string[]) => {
     // Note: this demo popup only supports a single selection
     const annotation = store.getAnnotation(selection[0]);
-    [left, top] = defaultStrategy(annotation, lastPointerDown);
+
+    const { minX, minY, maxX, maxY } = annotation.target.selector.geometry.bounds;
+
+    const PADDING = 14;
+
+    const topLeft = viewer.viewport.imageToViewerElementCoordinates(new OpenSeadragon.Point(minX, minY));
+    const bottomRight = viewer.viewport.imageToViewerElementCoordinates(new OpenSeadragon.Point(maxX, maxY));
+
+    // [left, top] = defaultStrategy(annotation, lastPointerDown);
+    left = bottomRight.x + PADDING;
+    top = topLeft.y;
   }
 
   onMount(() => {
-    const onPointerDown = (event: PointerEvent) => 
-      lastPointerDown = event;
+    const onUpdateViewport = () => {
+      if ($selection && !dragged)
+        setPosition($selection);
+    }
 
-    document.addEventListener('pointerdown', onPointerDown);
+    viewer.addHandler('update-viewport', onUpdateViewport);
 
     return () => {
-      document.removeEventListener('pointerdown', onPointerDown);
+      viewer.removeHandler('update-viewport', onUpdateViewport);
     }
   });
 </script>
 
 {#if $selection}
-  <div use:draggable={dragOpts} class="popup" style={`left: ${left}px; top: ${top}px`}>
+  <div 
+    class="a9s-popup a9s-osd-popup"
+    use:draggable={{ position: { x: left, y: top }}}
+    on:neodrag:start={onDragStart}
+    on:neodrag:end={onDragEnd}>
+
     {$selection.join(', ')}
+
   </div>
 {/if}
 
 <style>
-  .popup {
+  .a9s-osd-popup {
     background-color: #fff;
     border: 1px solid #a2a2a2;
     height: 250px;
