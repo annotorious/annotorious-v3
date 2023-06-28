de42725627630339d69b0b79ccf87f291f5bfb3c
Rainer Simon <rainer@rainersimon.io>
2023-06-28 08:33:25 +0200

    Fix: React package now properly exports OSD useViewer hook

--- packages/annotorious-react/src/openseadragon/index.ts
+++ packages/annotorious-react/src/openseadragon/index.ts
@@ -1,3 +1,4 @@
-export { OpenSeadragonAnnotator, type OpenSeadragonAnnotatorProps } from './OpenSeadragonAnnotator';
+export { OpenSeadragonAnnotator, useViewer } from './OpenSeadragonAnnotator';
+export type { OpenSeadragonAnnotatorProps } from './OpenSeadragonAnnotator';
 export { OpenSeadragonPopup, type OpenSeadragonPopupProps } from './OpenSeadragonPopup';
 export * from './OpenSeadragonViewer';
